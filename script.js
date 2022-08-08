70bdca6a381422ffc034a35e1a836dc078f8e842
Nchukwitony <110847136+Nchukwitony@users.noreply.github.com>
2022-08-08 18:37:14 +0100

    My commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " TIANGO Institude. All rights reserved.</p>";
+
+//start nav menu
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='customers.html'>Data</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
+
+//end of nav menu
